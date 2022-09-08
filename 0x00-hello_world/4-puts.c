c182785cefe12e1887d7677bf45ce17feb655e2f
Caesar-12 <idowuenoch65@gmail.com>
2022-09-08 16:58:49 -0700

    Compiled code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,6 @@
 /* Writing a simple C program*/
 int main (void)
 {
-  puts("\"Programming is like building a multilingual puzzle")
-    return 0
+  puts("\"Programming is like building a multilingual puzzle");
+  return 0;
 }
