9e9afcd34546d2a7df772b7733a1a917b393a9a7
Caesar-12 <idowuenoch65@gmail.com>
2022-09-08 19:43:48 -0700

    Figuring out descriptions

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,10 +2,8 @@
 
 /*
  * main - Entry point
- *
- * Description: using the main function
- *
  * Return: Always 0 (Success)
+ * Description -Size is not grandeur, and teritory does not make a nation
  */
 
 int main(void)
@@ -14,9 +12,10 @@ int main(void)
 	int four_bytes = 4;
 	int eight_bytes = 8;
 
-	printf("Size of a char: %d byte(s):\n", one_bytes);
-	printf("Size of a int: %d byte(s):\n", four_bytes);
-	printf("Size of a long int: %d byte(s):\n", four_bytes);
-	printf("Size of a long long int: %d byte(s):\n", eight_bytes);
-	printf("Size of a float: %d byte(s):\n", four_bytes);
+	printf("Size of a char: %d byte(s)\n", one_bytes);
+	printf("Size of a int: %d byte(s)\n", four_bytes);
+	printf("Size of a long int: %d byte(s)\n", four_bytes);
+	printf("Size of a long long int: %d byte(s)\n", eight_bytes);
+	printf("Size of a float: %d byte(s)\n", four_bytes);
+	return (o);
 }
