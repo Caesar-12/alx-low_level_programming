ae9937723007655d597285cb0baab61f4f076eed
Caesar-12 <idowuenoch65@gmail.com>
2022-09-08 19:52:45 -0700

    Modified

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,14 +8,16 @@
 
 int main(void)
 {
-	int one_bytes = 1;
-	int four_bytes = 4;
-	int eight_bytes = 8;
+	char c;
+	int i;
+	long l;
+	long long ll;
+	float f;
 
-	printf("Size of a char: %d byte(s)\n", one_bytes);
-	printf("Size of a int: %d byte(s)\n", four_bytes);
-	printf("Size of a long int: %d byte(s)\n", four_bytes);
-	printf("Size of a long long int: %d byte(s)\n", eight_bytes);
-	printf("Size of a float: %d byte(s)\n", four_bytes);
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of a int: %lu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 	return (0);
 }
