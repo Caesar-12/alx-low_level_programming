e392677b12b97985a03e8c5b1e8348f569c44ba4
Caesar-12 <idowuenoch65@gmail.com>
2022-09-09 05:07:32 -0700

    If conditional statement and rand func

---
+++ 0x01-variables_if_else_while/0-negative_or_positive.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - Entry point
+ *Description: Using the if conditional statement with the random func
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = rand();
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	} else if (n  < 0)
+	{
+		printf("%d is negative\n", n);
+	} else
+	{
+		printf("%d is zero\n", n);
+	}
+
+	return 0;
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,3 @@
+#if, for and while statements
+Working with the if conditional statement in C
+Working with for loops and while loops
