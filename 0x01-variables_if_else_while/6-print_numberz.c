c93eb252d5583526ac0a00ff76850f003eb204ef
Caesar-12 <idowuenoch65@gmail.com>
2022-09-09 14:41:27 -0700

    Printing numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: Printing alphabets using putchar function
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha[] = "0123456789";
+	int l = 0;
+
+	while (alpha[l] != '\0' && alpha[l] != '\n')
+	{
+		putchar(alpha[l]);
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
