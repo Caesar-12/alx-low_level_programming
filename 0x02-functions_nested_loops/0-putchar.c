53317dfefc7d5e708c10600e8a3423c33bb4f440
Caesar-12 <idowuenoch65@gmail.com>
2022-09-13 06:38:54 -0700

    C functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * _putchar - Entry point
+ * Return: on success 1
+ * @c: value to print
+ * Description: Write Function
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * Description: C funtions
+ */
+int main(void)
+{
+	int size;
+
+	size = _putchar('s');
+
+	putchar(size);
+	putchar('\n');
+
+	return (0);
+}
