11afdad6e11fc656a0c11b3b2059cfc98672be7a
Caesar-12 <idowuenoch65@gmail.com>
2022-09-13 18:50:20 -0700

    Modified var

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,19 +11,21 @@
 
 int print_sign(int n)
 {
-	if (n > 0)
+	int num = n;
+
+	if (num > 0)
 	{
 		putchar('+');
 		return (1);
 	}
-	else if (n < 0)
-	{
-		putchar('-');
-		return (-1);
-	}
-	else if (n == 0)
+	else if (num = 0)
 	{
 		putchar('0');
 		return (0);
+	}
+	else
+	{
+		putchar('-');
+		return (-1);
 	}
 }
