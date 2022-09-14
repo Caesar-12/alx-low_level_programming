6be8b5f87b4d176d891f4e69b94aa0df9f84b8c2
Caesar-12 <idowuenoch65@gmail.com>
2022-09-13 21:13:34 -0700

    Sums

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add - Main body
+ * Description: Sum up input
+ * @first: first integer to sum
+ * @second: second integer
+ * Return: Sum of input
+ */
+
+int add(int first, int second)
+{
+	return (first + second);
+}
