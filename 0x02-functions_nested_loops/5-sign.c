050cc368f59db6d64e00d1c5d353a5c828648e40
Caesar-12 <idowuenoch65@gmail.com>
2022-09-13 18:37:03 -0700

    Printing number signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_sign - Body
+ *
+ * Description: Prints out the sign of a number
+ * @n: value to be received
+ * Return: 1 if greater than 0, 0 if equal to 0, -1 if lesser than 0
+ */
+
+int print_sign(int n)
+{
+	int num = n;
+
+	if (num > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (num < 0)
+	{
+		putchar('-');
+		return (-1);
+	}
+	else if (num == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+}
