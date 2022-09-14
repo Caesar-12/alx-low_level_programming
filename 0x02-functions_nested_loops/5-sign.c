1073dc4a769c95f51bf1362585563aa39e288b2f
Caesar-12 <idowuenoch65@gmail.com>
2022-09-13 18:44:40 -0700

    Printing number signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,19 +11,17 @@
 
 int print_sign(int n)
 {
-	int num = n;
-
-	if (num > 0)
+	if (n > 0)
 	{
 		putchar('+');
 		return (1);
 	}
-	else if (num < 0)
+	else if (n < 0)
 	{
 		putchar('-');
 		return (-1);
 	}
-	else if (num == 0)
+	else if (n == 0)
 	{
 		putchar('0');
 		return (0);
