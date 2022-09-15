0817ed496bb70bf03a9cdcb4c70e2f2cc195730f
Caesar-12 <idowuenoch65@gmail.com>
2022-09-15 05:15:42 -0700

    Products

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Function body
+ * Description: Multiplies received digits
+ * @a: first int
+ * @b: second int
+ * Return: Result of product
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
