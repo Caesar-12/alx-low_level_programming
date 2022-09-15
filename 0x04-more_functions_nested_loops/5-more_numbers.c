b94e9b8cae10968635c3246461f4585f5fd06fc2
Caesar-12 <idowuenoch65@gmail.com>
2022-09-15 14:11:30 -0700

    Mods

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,15 +16,11 @@ void more_numbers(void)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			if (i <= 9)
-			{
-				_putchar('0' + i);
-			}
-			else
+			if ((i / 10) > 0)
 			{
 				_putchar('0' + (i / 10));
-				_putchar('0' + (i % 10));
 			}
+			_putchar('0' + (i % 10));
 		}
 		_putchar('\n');
 	}
