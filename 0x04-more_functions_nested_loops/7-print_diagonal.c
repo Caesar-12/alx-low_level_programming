a43ec64072b6ba711271cc0630ac05975e279129
Caesar-12 <idowuenoch65@gmail.com>
2022-09-15 15:08:12 -0700

    Diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - Main body
+ * Description: Piths diagonals
+ * @n: Int representing diagonal lenght
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int num;
+	int numb = n;
+	int space;
+
+	for (num = 0; num < n; num++)
+	{
+		for (space = 0; space <= num; space++)
+		{
+			if (numb > 0)
+			{
+				_putchar(' ');
+			}
+		if (numb > 0)
+		{
+			_putchar('\\');
+			_putchar('\n');
+		}
+		}
+	}
+	_putchar('\n');
+}
