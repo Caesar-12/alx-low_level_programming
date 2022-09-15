97ce8466aedb28de512871341c0f5c532e827e97
Caesar-12 <idowuenoch65@gmail.com>
2022-09-14 19:35:22 -0700

    Multiplication tables of user defined size

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_times_table - Main computing body
+ * Description: Print multiplication table up to received integer
+ * @n: Int to be received
+ */
+
+void print_times_table(int n)
+{
+	int down;
+	int across;
+	int times;
+
+	for (across = 0; across <= n; across++)
+	{
+		for (down = 0; down <= n; down++)
+		{
+			times = across * down;
+
+			if (times <= 9)
+			{
+				putchar('0' + times);
+				putchar(',');
+				putchar(' ');
+				putchar(' ');
+				putchar(' ');
+			}
+			else if (times > 9 && times <= 99)
+			{
+				putchar('0' + times / 10);
+				putchar('0' + times % 10);
+				putchar(',');
+				putchar(' ');
+				putchar(' ');
+			}
+			else if (times > 99)
+			{
+				putchar('0' + times / 100);
+				putchar('0' + (times / 10) % 10);
+				putchar('0' + times % 10);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+	}
+}
