7f951da4e49d53ad476d918a2759b9b9938ec6d3
Caesar-12 <idowuenoch65@gmail.com>
2022-09-15 09:45:50 -0700

    Printing n number of lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - Main body
+ * Description: Print n number of lines
+ * @n: Int to receive
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n > 0)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
