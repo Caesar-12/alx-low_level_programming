4ff62187b766a6013135c0f6a99cf655568bca2e
Caesar-12 <idowuenoch65@gmail.com>
2022-09-15 06:41:37 -0700

    Numbers with exception

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - Main body
+ * Description: Prints int except 2 and 4
+ * Return: Void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+}
