6c82e223875ebbf880e14f465bbaeb352f0ff307
Caesar-12 <idowuenoch65@gmail.com>
2022-09-15 07:30:49 -0700

    Modifications

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,14 +9,13 @@
 
 void print_most_numbers(void)
 {
-	int i;
+	int i = 0;
+	char num[] = "01356789";
 
-	for (i = 0; i <= 9; i++)
+	while (num[i] != '\0')
 	{
-		if (i != 2 || i != 4)
-		{
-			_putchar(i);
-		}
+		_putchar(num[i]);
+		i++;
 	}
 	_putchar('\n');
 }
