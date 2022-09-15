b87926d39489e43f3658ff214c63f29f3190c6fd
Caesar-12 <idowuenoch65@gmail.com>
2022-09-15 05:23:38 -0700

    Printing digits

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Func body
+ * Description: Prints number 0-9
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0' + i);
+	}
+}
