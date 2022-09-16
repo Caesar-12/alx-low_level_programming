525c5eb34235d42e8603a7649987c26e9728a61c
Caesar-12 <idowuenoch65@gmail.com>
2022-09-15 18:50:44 -0700

    Debugging given functions

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Check if func runs properly
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 25;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+Debugging code functions
+
