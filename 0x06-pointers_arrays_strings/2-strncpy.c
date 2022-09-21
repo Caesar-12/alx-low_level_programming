2dfcf4eb9eab8c99b3f66f7c1822397f687545fa
Caesar-12 <idowuenoch65@gmail.com>
2022-09-21 16:49:59 -0700

    Copying strings without the stdlib

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strncpy - Func body
+ * Description: Copies a string into another
+ * @dest: Empty string to copy into
+ * @src: String to copy from
+ * @n: Number of characters to copy
+ * Return: Returns copied string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int  len = 0;
+
+	while (src[len] != '\0' && len < n)
+	{
+		dest[len] = src[len];
+		len++;
+	}
+
+	dest[len] = '\0';
+	return (dest);
+}
