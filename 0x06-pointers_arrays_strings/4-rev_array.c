06bfd0b3cb433ba4754a191e9179ffd7a75af06a
Caesar-12 <idowuenoch65@gmail.com>
2022-09-21 18:16:09 -0700

    Reversing contents in an array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,6 +3,7 @@
 
 /**
  * reverse_array - Func body
+ * Description: Reverse int order in an array
  * @a: Array to reverse content
  * @n: Number of int in array a
  * Return: void
@@ -17,6 +18,7 @@ void reverse_array(int *a, int n)
 	{
 		rev = a[len];
 		a[len] = a[n];
+		a[n] = rev;
 		n--;
 	}
 }
