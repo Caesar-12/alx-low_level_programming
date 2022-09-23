027d6a2caa5debe1392771377e6146a45ca3827c
Enoch Idowu <111004428+Caesar-12@users.noreply.github.com>
2022-09-23 01:51:39 +0100

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,36 +2,36 @@
 #include "main.h"
 
 /**
- * rot13 - Encodes with rot-13 cypher
- * @c: String to encode
- * Return: Encoded string
+ * rot13 - Encodes string using rot13
+ * @s: string to encode
+ * Return: encoded string
  */
 
-char *rot13(char *c)
+char *rot13(char *s)
 {
-	int i;
-	int len;
-	int alpha[53] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-		'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-		'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-	int encode[53] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-		'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-		'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B',
-		'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
 
-	for (i = 0; c[i] != '\0'; i++)
+	int a[53] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+		     'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		     'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+		     'W', 'X', 'Y', 'Z'};
+	int b[53] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		     'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+		     'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+		     'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+		     'J', 'K', 'L', 'M'};
+
+	int i, j;
+
+	for (j = 0; s[j] != '\0'; j++) /*loop through string*/
 	{
-		len = 0;
+		i = 0;
+		while (a[i] != '\0' && s[j] != a[i]) /*loop through rot13 arr*/
+			i++;
 
-		while (alpha[len] != '\0' && c[i] != alpha[len])
-		{
-			len++;
-		}
-		if (c[i] != alpha[len])
-		{
-			c[i] = encode[len];
-		}
+		if (s[j] == a[i]) /*if alpha matches, set to index in b arr*/
+			s[j] = b[i];
 	}
-	return (c);
+
+	return (s);
 }
