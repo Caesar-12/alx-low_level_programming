bd338f488b94f55c3c1fdcee2a853fe809f6b8b9
Caesar-12 <idowuenoch65@gmail.com>
2022-09-26 14:28:24 -0700

    Locate character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strchr - Locates first experience
+ * @s: Pointer to receive first occurence of char c
+ * @c: Character to locate
+ * Return: Returns pointer s
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			s = (&s[i]);
+			return (s);
+		}
+		i++;
+	}
+}
