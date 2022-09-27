e85cd791617336f548b83307361c260ccc0fe8da
Enoch Idowu <111004428+Caesar-12@users.noreply.github.com>
2022-09-27 03:01:22 +0100

    Searching for first occurrence of a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,6 +8,31 @@
  * Return: Pointer to the substring
  */
 
-char *_strstr(char *haystack, char *needle);
+char *_strstr(char *haystack, char *needle)
 {
+	int l = 0, j, x;
 
+	if (needle[0] == '\0')
+                return (haystack);
+
+	while (haystack[i] != '\0')
+	{
+		if (haystack[i] == needle[0])
+		{
+			x = i, j = 0;
+			while (needle[j] != '\0')
+			{
+				if (haystack[x] == needle[j])
+					x++, j++;
+				else
+					break;
+			} 
+			if (needle[j] == '\0')
+			{
+				return (haystack + i);
+			}
+		}
+		i++;
+	}
+	return ('\0');
+}
