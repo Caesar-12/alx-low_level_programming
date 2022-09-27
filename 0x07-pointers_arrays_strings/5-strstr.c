d5fb68a555cf5709f1f118eaf2f2902ba3bdbe3f
Caesar-12 <idowuenoch65@gmail.com>
2022-09-26 18:52:54 -0700

    Unfinished strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strstr - Locates the first occurence of a string(word)
+ * @haystack: main string
+ * @needle: String to search
+ * Return: Pointer to the substring
+ */
+
+char *_strstr(char *haystack, char *needle);
+{
+
