46e6b4264e4f8aa93753bde0c7bb05cbc45ecffa
Caesar-12 <idowuenoch65@gmail.com>
2022-09-28 17:20:21 -0700

    Factorials of given int using recursions

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * factorial - Gives the factorial of a given int
+ * @n: Number to evaluate factorial
+ * Return: Factorial/
+ */
+
+int factorial(int n)
+{
+	if (n == 1)
+	{
+		return (1);
+	}
+
+	return (n * factorial(n - 1));
+}
