f3ce40d2576b2f51eb97bd7f262ec9bf51615055
Caesar-12 <idowuenoch65@gmail.com>
2022-09-28 17:38:11 -0700

    Indices with recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,11 +10,18 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (y == 1)
+	if (x == 0)
 	{
-		return (x);
+		return (0);
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+	else if (x == 1 || y == 0)
+	{
+		return (1);
 	}
 
-	return (x * _pow_recursion(x, y));
-	y--;
+	return (x * _pow_recursion(x, y - 1));
 }
