4451961d3859f9bd8ffb778b28646e0dbed9d70a
Caesar-12 <idowuenoch65@gmail.com>
2022-09-28 18:03:05 -0700

    Natural square root of a given int using recursions

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * square_root - Gets the square root of given int
+ * @n: int to get square root
+ * @root: Actual root
+ * Return: Actual root of given int
+ */
+
+int square_root(int n, int root)
+{
+	if ((root * root) == n)
+	{
+		return (root);
+	}
+	else if ((root * root) > n)
+	{
+		return (-1);
+	}
+
+	return (square_root(n, root - 1));
+}
+
+/**
+ * _sqrt_recursion - Calculates the natural square root of a number
+ * @n: Int to calculate square root
+ * Return: Natural square root ofgiven int
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (square_root(n, 0));
+	}
+}
