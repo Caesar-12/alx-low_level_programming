d7397e9ec1010e133c429095a83719a48ff25666
Caesar-12 <idowuenoch65@gmail.com>
2022-09-30 13:24:53 -0700

    Command line arguments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - Prints a program name
- * Return: 0
  * @argv: Argument
  * @argc: Argument count
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
