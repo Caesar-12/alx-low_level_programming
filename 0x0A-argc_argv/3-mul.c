abb1886c541c8f60fdf96df0da019637ea898514
Caesar-12 <idowuenoch65@gmail.com>
2022-09-30 14:26:03 -0700

    Multiplying int arg

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints a program name
+ * @argv: Argument
+ * @argc: Argument count
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, product;
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	product = num1 + num2;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	if (argc == 3)
+	{
+		printf("%d\n", product);
+	}
+	return (0);
+}
