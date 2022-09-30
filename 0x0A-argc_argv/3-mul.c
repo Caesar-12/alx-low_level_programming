99895fcd15734d8178a33a261e2166d95af571b5
Caesar-12 <idowuenoch65@gmail.com>
2022-09-30 14:45:26 -0700

    Multiplying int arg

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,16 +12,15 @@ int main(int argc, char *argv[])
 {
 	int num1, num2, product;
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-
-	product = num1 * num2;
-
 	if (argc != 3)
 	{
 		printf("%s\n", "Error");
 		return (1);
 	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	product = num1 * num2;
 
 	printf("%d\n", product);
 
