0205f440aec1000230876e139f8af50a03b6d7c3
Caesar-12 <idowuenoch65@gmail.com>
2022-10-10 15:51:16 -0700

    Structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef STRUCTURES
+#define STRUCTURES dog.h
+
+/**
+ * struct dog - Defines a structure
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
