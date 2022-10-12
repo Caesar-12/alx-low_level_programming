ac4ff89e8416974a3a48f52a7bd2a7a525fa6ace
Caesar-12 <idowuenoch65@gmail.com>
2022-10-12 03:06:44 -0700

    call-back func

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Executes a function on each element
+ * @array: Array to work on elements
+ * @size: Size of array
+ * @action: Pointer to func to execute
+ *
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < (int)size; i++)
+	{
+		(*action)(*(array + i));
+	}
+}
